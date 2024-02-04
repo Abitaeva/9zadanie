51bad801205459fa56ef31e620a48f8e609e96e8
Абитаева <anazyken@gmail.com>
2024-02-04 23:11:56 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Naz.com'),
+        ),
+        body: Image.asset("assets/images/images (2).png"),
+      ),
+    ),
+  );
+}
